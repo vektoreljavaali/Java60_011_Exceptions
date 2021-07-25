c4ee6659f7bedf31e677caadd99c5c3a6100f15a
Vektorel <Vektorel@LAB8-OGRETMEN>
2021-07-25 17:51:36 +0300

    Soru içeriği eklendi

---
+++ src/com/vektorel/SORU.java
@@ -0,0 +1,39 @@
+package com.vektorel;
+
+public class SORU {
+
+	public static void main(String[] args) {
+
+		// .   .   .
+		//
+		// .   *   .
+		//
+		// .   .   .
+		//
+		//
+		//     8
+		//  4     6
+		//     2
+		// rakamlar yön tuþudur.
+		// // örnek:  6 ya bastý
+		// .  .  .
+		//
+		// .  .  *
+		//
+		// .  .  .
+		// // tekrar 6 ya bastýðýnda baþa dönecek
+		//
+		// .  .  .
+		//
+		// *  .  .
+		//
+		// .  .  .
+		
+		
+		
+
+		
+
+	}
+
+}
